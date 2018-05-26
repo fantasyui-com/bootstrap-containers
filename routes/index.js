00236dccdea78b501a09fbaf3c8e410a2237c966
fantasyui-com <captain@fantasyui.com>
2018-05-26 09:23:26 -0400

    Generator Integration

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var generator = require('container-generator');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bootstrap Containers' });
+
+  const containers = [];
+  for(let i=0;i<10;i++){
+    containers.push(generator())
+  }
+
+  res.render('index', { title: 'Bootstrap Containers', containers });
 });
 
 module.exports = router;
